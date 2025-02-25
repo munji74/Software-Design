409b35cf89c6e662add12d6ce6a29fb6dd770e11
Munjwok James Alala <habib.munji@gmail.com>
2025-02-25 09:14:14 +0300

    Created TaskPage and UserPage

---
+++ frontend/src/pages/TasksPage.js
@@ -0,0 +1,12 @@
+import TaskList from "../components/TaskList";
+
+const TasksPage = () => {
+    return (
+        <div>
+            <h1>Task Management</h1>
+            <TaskList />
+        </div>
+    );
+};
+
+export default TasksPage;
