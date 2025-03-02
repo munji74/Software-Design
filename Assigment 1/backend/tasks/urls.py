59901b4511d12781530748b8f9a3cb7343b26846
Munjwok James Alala <habib.munji@gmail.com>
2025-03-02 19:19:17 +0300

    Implemented Task Management

--- Assigment 1/backend/tasks/urls.py
+++ Assigment 1/backend/tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TaskListCreateView
+from .views import TaskListCreateView, TaskDetailView
 
 urlpatterns = [
-    path('', TaskListCreateView.as_view(), name='task-list-create'),
+    path('', TaskListCreateView.as_view(), name='task-list-create'),  # Handles GET (list) & POST (create)
+    path('<int:pk>/', TaskDetailView.as_view(), name='task-detail'),  # Handles GET (retrieve), PUT (update), DELETE (remove)
 ]
--- Assigment 1/backend/tasks/views.py
+++ Assigment 1/backend/tasks/views.py
@@ -5,3 +5,7 @@ from .serializers import TaskSerializer
 class TaskListCreateView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
